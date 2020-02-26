b0f596a2910c63c1c0808db6f341b921c47c3a51
benja-github <ben.legge1@gmail.com>
2020-02-26 07:03:45 -0500

    Initial upload of files following repo creation

---
+++ SnowflakeConnectorPyAdf/SnowflakeConnectorPyAdf.py
@@ -0,0 +1,8 @@
+__validBlobFolderNameRegex = @"^[A-Za-z0-9_-]+$";
+# The parameter name corresponds to a restricted Snowflake unquoted identifier
+# https://docs.snowflake.net/manuals/sql-reference/identifiers-syntax.html
+__validParameterNameRegex = @"^[A-Za-z_]{1}[A-Za-z0-9_-]*$";
+# This is pretty restrictive
+__validParameterTypeRegex = @"^VARCHAR|NUMBER$";
+__validParameterValueRegex = @"^[A-Za-z0-9./\\ :_-]+$";
+
