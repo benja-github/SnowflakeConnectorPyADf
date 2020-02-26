136fb78a761e1b4698cdce5d91ae8eaa589a4cb9
benja-github <ben.legge1@gmail.com>
2020-02-26 12:45:42 -0500

    add very basic logging function

--- SnowflakeConnectorPyAdf/SnowflakeConnectorPyAdf.py
+++ SnowflakeConnectorPyAdf/SnowflakeConnectorPyAdf.py
@@ -1,5 +1,5 @@
 import logging
-
+from datetime import datetime
 import azure.functions as func
 
 __validBlobFolderNameRegex = r'^[A-Za-z0-9_-]+$'
@@ -10,6 +10,13 @@ __validParameterNameRegex = r'^[A-Za-z_]{1}[A-Za-z0-9_-]*$'
 __validParameterTypeRegex = r'^VARCHAR|NUMBER$'
 __validParameterValueRegex = r'^[A-Za-z0-9./\\ :_-]+$'
 
+def write_to_log(message,severity='INFO'):
+    now = datetime.now()
+    now_string = now.strftime('%d-%m-%Y, %H:%M:%S')
+    log_message='{0}::{1} (UTC): {2}'.format(severity,now_string,message)
+    logging.info(log_message);
+
 def Run(req: func.HttpRequest):
+    write_to_log('Started funcion Run()');
     req_body=str(req.get_body(),'UTF-8')
     return req_body
