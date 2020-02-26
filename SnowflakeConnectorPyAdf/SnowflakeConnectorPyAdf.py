b921a2fc1ae0759fa0f7a9b87d3f1f113ad5cbda
benja-github <ben.legge1@gmail.com>
2020-02-26 12:11:46 -0500

    Project accepting http requests & return something

--- SnowflakeConnectorPyAdf/SnowflakeConnectorPyAdf.py
+++ SnowflakeConnectorPyAdf/SnowflakeConnectorPyAdf.py
@@ -1,9 +1,15 @@
-__validBlobFolderNameRegex = @"^[A-Za-z0-9_-]+$";
+import logging
+
+import azure.functions as func
+
+__validBlobFolderNameRegex = r'^[A-Za-z0-9_-]+$'
 # The parameter name corresponds to a restricted Snowflake unquoted identifier
 # https://docs.snowflake.net/manuals/sql-reference/identifiers-syntax.html
-__validParameterNameRegex = @"^[A-Za-z_]{1}[A-Za-z0-9_-]*$";
+__validParameterNameRegex = r'^[A-Za-z_]{1}[A-Za-z0-9_-]*$'
 # This is pretty restrictive
-__validParameterTypeRegex = @"^VARCHAR|NUMBER$";
-__validParameterValueRegex = @"^[A-Za-z0-9./\\ :_-]+$";
+__validParameterTypeRegex = r'^VARCHAR|NUMBER$'
+__validParameterValueRegex = r'^[A-Za-z0-9./\\ :_-]+$'
 
-
+def Run(req: func.HttpRequest):
+    req_body=str(req.get_body(),'UTF-8')
+    return req_body
--- SnowflakeConnectorPyAdf/__init__.py
+++ SnowflakeConnectorPyAdf/__init__.py
@@ -2,23 +2,27 @@ import logging
 
 import azure.functions as func
 
+# Following needed, at least locally, to import modules that
+# are part of project (i.e. in same location as __inti__.py)
+import os
+import sys
+from sys import path
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, dir_path)
+# import of project modules must go below here
+
+import SnowflakeConnectorPyAdf
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    request_body_string = req.Body
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    ret_val = SnowflakeConnectorPyAdf.Run(req)
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    #if name:
+    return '---'+ret_val+'|||||' #func.HttpResponse(f"Hello Dave!")
+    #else:
+    #    return func.HttpResponse(
+    #         "Please pass a name on the query string or in the request body",
+    #         status_code=400
+    #    )
